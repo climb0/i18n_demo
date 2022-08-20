2f52fb9ddc8a9a7bb5d4820049039b86813d389f
yx <568796191@qq.com>
2022-08-21 00:29:43 +0800

    add

---
+++ src/main/java/annotation/I18nMethod.java
@@ -0,0 +1,10 @@
+package annotation;
+
+/**
+ * @author yx
+ * @date 2022/8/21
+ * @desc
+ */
+
+public @interface I18nMethod {
+}
---
+++ src/main/java/aspect/I18nAspect.java
@@ -0,0 +1,10 @@
+package aspect;
+
+/**
+ * @author yx
+ * @date 2022/8/21
+ * @desc
+ */
+
+public class I18nAspect {
+}
